c8dbe96064f1a976778fda5f18ddd27df7b2813b
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2019-11-20 10:13:07 +0100

    remove Foo controller

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,3 @@
-INSERT INTO FOO(id, name, created_at, updated_at) VALUES(1, 'Kriszti�n', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO FOO(id, name, created_at, updated_at) VALUES(2, '�d�m', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO FOO(id, name, created_at, updated_at) VALUES(3, 'M�rk', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-
 insert into issue (title, description, location, status, created_at, updated_at) values ('The issue #1', 'Fatallica', 'PC42', 'NEW', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
 insert into issue (title, description, location, status, created_at, updated_at) values ('MegaIssue', 'MegaFatal', 'PC12', 'INPROGRESS', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
 insert into issue (title, description, location, status, created_at, updated_at) values ('Unknown issue', 'U.F.O', 'PC11', 'INPROGRESS', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
